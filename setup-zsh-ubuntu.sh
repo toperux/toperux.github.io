51d4506fc1cdb242cf7c57fc8ca7ecb9a2f702d9
Christopher Montevirgen <montevirgen@dxc.com>
2020-05-08 03:18:53 +0800

    update scripts

--- setup-zsh-ubuntu.sh
+++ setup-zsh-ubuntu.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo usermod -s `which zsh` $USER
-echo "alias apt-update=\"sudo apt update && sudo apt upgrade -y\"" >> ~/.zshrc
+#sudo usermod -s `which zsh` $USER
+#echo "alias apt-update=\"sudo apt update && sudo apt upgrade -y\"" >> ~/.zshrc
